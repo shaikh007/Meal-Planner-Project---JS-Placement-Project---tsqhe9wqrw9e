92638f7171a9665f754dfde45a290e114e89f75d
Shaikh Irfan <shaikhji1998@gmail.com>
2023-08-03 19:37:55 +0530

    new commit

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 //contact Send email
+
+
 function SendMail(event) {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const mobile = document.getElementById("mobile");
+    const message = document.getElementById("message");
     event.preventDefault();
-    var params = {
-        from_name: document.getElementById("name").value,
-        email_id: document.getElementById("email").value,
-        mobile_no: document.getElementById("mobile").value,
-        message: document.getElementById("message").value
+    if (name.value === "") {
+        alert("please fill the name");
+    } else if (email.value === "") {
+        alert("please fill email address");
+    } else if (mobile.value === "") {
+        alert("please fill the mobile number");
+    } else if (message.value === "") {
+        alert("please fill atleast 10 words")
+    } else {
+        var params = {
+            from_name: name.value,
+            email_id: email.value,
+            mobile_no: mobile.value,
+            message: message.value
+        }
+        emailjs.send("service_jgawk9s", "template_bs73s3k", params).then(function(res) {
+            alert("Sent Successfully 😇 " + res.status);
+        })
     }
-    emailjs.send("service_jgawk9s", "template_bs73s3k", params).then(function(res) {
-        alert("Sent Successfully 😇 " + res.status);
-    })
+
     console.log(params);
 }
